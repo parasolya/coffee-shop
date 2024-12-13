ec17f3da9846a0cdbb898596db8e97eddc69ecc1
parasolya <ola.shevchuk@gmail.com>
2024-12-13 12:37:53 +0200

    fixed styles for mobile

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="section relative z-10 w-full h-[600px] bg-cover bg-center" style={{ backgroundImage: "url('/hero.jpg')"}}
+    <section className="section relative z-10 w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url('/hero.jpg')"}}
     >
       <div className="inset-0 bg-black bg-opacity-50"></div>
       <div className="container flex flex-col md:flex-row items-center justify-between text-center md:text-left text-white px-6 md:px-12">
